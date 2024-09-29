d63f8c9a8bc70596387c1927a70d29c4d06df772
Harshit Bhardwaj <harshit.bhardwaj@nagarro.com>
2024-09-29 22:38:36 +0530

    added a launch script and added new some UI changes

--- vcs-front/src/VcsComponents/PullComponent/DiffComponent.js
+++ vcs-front/src/VcsComponents/PullComponent/DiffComponent.js
@@ -217,7 +217,7 @@ export default function DiffComponent() {
                     <div className='card card-body bg-default badge mb-2'>
                         <MarkdownBlock classes={'text-light'} content={createMarkdownStatement(data.branchName, data.branchCommitHash, data.baseBranchName, data.baseBranchCommitHash)} />
                     </div>
-                    <div className="text-light m-2">
+                    {/* <div className="text-light m-2">
                         {isMergeable ?
                             <button className='btn btn-success btn-sm badge position-relative'>
                                 <svg fill="#fff" width="20" height="20" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
@@ -229,7 +229,7 @@ export default function DiffComponent() {
                             : <span className='text-bg-danger badge'>
                                 Merge Conflict 
                             </span>}
-                    </div>
+                    </div> */}
                     <table className='table table-dark'>
                         <thead>
                             <tr className="text-light">
@@ -276,12 +276,11 @@ export default function DiffComponent() {
                             )}
                         </tbody>
                     </table>
-                    <hr className='text-light' />
-                    <div className="mt-4">
+                    <div className="mt-4 card card-body bg-default">
                         {mergedLines.length > 0 ? (
                             <>
-                                <h4 className='text-light'><svg fill="#fff" width="20" height="20" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-d</title><path d="M218.31,340.69A16,16,0,0,0,191,352v32H171a28,28,0,0,1-28-28V152a64,64,0,1,0-64-1.16V356a92.1,92.1,0,0,0,92,92h20v32a16,16,0,0,0,27.31,11.31l64-64a16,16,0,0,0,0-22.62ZM112,64A32,32,0,1,1,80,96,32,32,0,0,1,112,64Z" /><path d="M432,360.61V156a92.1,92.1,0,0,0-92-92H320V32a16,16,0,0,0-27.31-11.31l-64,64a16,16,0,0,0,0,22.62l64,64A16,16,0,0,0,320,160V128h20a28,28,0,0,1,28,28V360.61a64,64,0,1,0,64,0ZM400,448a32,32,0,1,1,32-32A32,32,0,0,1,400,448Z" /></svg> Save Final State</h4>
-                                <table className="table table-dark table-hover">
+                                <h4 className='text-light font-raleway'><svg fill="#fff" width="20" height="20" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-d</title><path d="M218.31,340.69A16,16,0,0,0,191,352v32H171a28,28,0,0,1-28-28V152a64,64,0,1,0-64-1.16V356a92.1,92.1,0,0,0,92,92h20v32a16,16,0,0,0,27.31,11.31l64-64a16,16,0,0,0,0-22.62ZM112,64A32,32,0,1,1,80,96,32,32,0,0,1,112,64Z" /><path d="M432,360.61V156a92.1,92.1,0,0,0-92-92H320V32a16,16,0,0,0-27.31-11.31l-64,64a16,16,0,0,0,0,22.62l64,64A16,16,0,0,0,320,160V128h20a28,28,0,0,1,28,28V360.61a64,64,0,1,0,64,0ZM400,448a32,32,0,1,1,32-32A32,32,0,0,1,400,448Z" /></svg> Commit New State</h4>
+                                <table className="table table-dark table-hover mb-4">
                                     <tbody>
                                         {
                                             mergedLines.map((line, index) => (
@@ -296,7 +295,7 @@ export default function DiffComponent() {
                                 </table>
                                 {
                                     isMergeable ? (
-                                        <div className='card car-body bg-dark text-light font-montserrat'>
+                                        <div className='bg-dark text-light font-montserrat'>
                                             <div class="input-group">
                                                 <span className="form-floating">
                                                     <input
